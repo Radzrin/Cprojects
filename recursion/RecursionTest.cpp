accce3296b70e826ab9a38726658431ba820a847
Radzrin <60394053+Radzrin@users.noreply.github.com>
2026-05-17 21:14:05 -0400

    exponent

--- recursion/RecursionTest.cpp
+++ recursion/RecursionTest.cpp
@@ -2,15 +2,33 @@
 
 
 int fibunacci(int n){
-    if(n <= 0)
+    if(n <= 1)
         return n;
 
     return fibunacci(n - 1) + fibunacci(n - 2);
 }
 
+double power(double base, int exponent){
+    if(exponent == 0)
+    return 1.0;
+
+    if(exponent < 0){
+        base = 1 / base;    
+        exponent *= -1; 
+    }
+
+    if(exponent == 1)
+        return base;
+
+    base *= power(base , exponent -= 1);
+
+    return base;
+}
+
 
 
 int main(){
-    std::cout << fibunacci(10);
+   //std::cout << fibunacci(10) << "\n";
+    std::cout << power(2,10);
     return 0;
 }
