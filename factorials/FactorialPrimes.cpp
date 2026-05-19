142d6d36d379f4f932a6a72b515d2b3a83d88e3e
Radzrin <60394053+Radzrin@users.noreply.github.com>
2026-05-18 23:39:37 -0400

    added comments

--- factorials/FactorialPrimes.cpp
+++ factorials/FactorialPrimes.cpp
@@ -2,49 +2,120 @@
 #include <iostream>
 #include <cstdint>
 #include <vector>
-#include <cmath>
+#include <algorithm>
 
 
-std::string string64(std::vector<std::string> in){
-    std::string big128Int = "";
-    std::vector<std::string> nv;
-    int w = in.size() - 1;
-    long long int sum = 0;
+std::string add(std::string n1, std::string  n2){
     int remainder = 0;
+    std::string res;
+    int i = n1.size();
+    int j = n2.size();
 
+    while(i != 0 || j != 0){
+        int sum;
+        if(i != 0)
+            sum += n1[i--] - '0';
+
+        if(j != 0)
+            sum += n2[j--] - '0';    
+
+        remainder = sum / 10;
+        res += (sum  % 10) - '0';    
+    }
+
+    std::reverse(res.begin(), res.end());
+    return res;
+}
+
+std::string subtract(std::string  n1, std::string n2){
+    std::string temp;
+    int borrow = 0;
+
+    if(n2 > n1){
+        temp = n1;
+        n1 = n2;
+        n2 = n1;
+    }
+
+    std::string res;
+
+    int i = n1.size();
+    int j = n2.size();
+
+    while(i >= 0 || j >= 0){
+        int dif = (n1[i--] - '0') - (n2[j--] - '0') - borrow;
+        if(dif < 0){
+            dif += 0;
+            borrow = 1;
+        }else{
+            borrow = 0;
+        }
+
+        res += borrow + '0'; 
+    }
+
+    std::reverse(res.begin(), res.end());
+    
+    return res;
+}
+
+/**
+ * using the karatsuba algorithm to calculate the sum of large integers
+ * the function accepts a string type vector and calculate the sum of the last 2 elements
+ * the function iterates through the vector backwards
+ * the karatsuba algorithm is a command and conquor algorithm similar to merge sort that
+ * takes 2 strings halves them recursively and multiplies them
+ * 
+ * for example if given a list: {123, 54, 2, 10} the function should do the following:
+ * 
+ * multiply 2*10 
+ * remove the last 2 elements from the vector
+ * add the sum to the back of the vector 
+ * and perform a recursion until only 1 element remains
+ */
+std::string karatsubaString(std::vector<std::string> in){
     if(in.size() == 1){
         return in.at(0);
     }
 
-    while(in.size() > 0){
-        for (int i = in.at(w).length() - 1; i >= 0; i--){
-            for (int j = in.at(w - 1).length() - 1; j >= 0; j--){
-
-                    if(in.at(w - 1).length()  > in.at(w).length() ){
-                        sum +=  ((in.at(w)[i]- '0') * (in.at(w - 1)[j] - '0')) * (p) * pow(10, in.at(w).length() - 1 - i));
-                    }else{
-                        sum +=  ((in.at(w)[i]- '0') * (in.at(w - 1)[j] - '0')) * (pow(10, in.at(w).length() - 1 - i) * pow(10, in.at(w - 1).length() - 1 - j));
-                    }
-            }
-        }
-        in.pop_back();
-        in.pop_back();
-
-        big128Int = std::to_string(sum);
-        nv.push_back(big128Int);
-        
-        if(in.size() == 1){
-            nv.push_back(in.at(0));
-            in.pop_back();
-        }
-
-        w = in.size() - 1;
-        sum = 0;
-    }
-
+    std::vector<std::string> nv;
+    std::string x = in.at(in.size() - 1);
+    std::string y = in.at(in.size() - 2);
     
+    int sz = std::max(x.size(), y.size());
 
-    return string64(nv);
+    if (sz == 1) 
+    return std::to_string((x[0] - '0') * (y[0] - '0'));
+
+    int halfX = x.size()/2;
+    int halfY = y.size()/2;
+    std::string x1 = x.substr(0, halfX);
+    std::string x2 = x.substr(halfX);
+    std::string y1 = y.substr(0, halfY);
+    std::string y2 = y.substr(halfY);
+
+    nv.push_back(x1);
+    nv.push_back(y1);
+    std::string ac = karatsubaString(nv);
+    nv.front() = x2;
+    nv.back()  = y2;
+    std::string bd = karatsubaString(nv);
+    
+    nv.front() = add(x1,x2);
+    nv.back()  = add(y1, y2);
+
+    std::string add_ab = karatsubaString(nv);
+
+    std::string subt = subtract(add_ab, add(ac, bd));
+
+    nv.pop_back();
+    nv.pop_back();
+    std::string finsum = add(add(ac + std::string(2 * (sz - halfX), '0'), subt + std::string(sz - halfY, '0')), bd);
+    in.pop_back();
+    in.pop_back();
+    in.push_back(finsum);
+
+    return karatsubaString(in);
 }
 
 
@@ -69,7 +140,7 @@ std::string factorial(int factorial){
     }
     
     
-    return string64(nv);
+    return karatsubaString(nv);
 }
 
 std::string decomp(int n) {
@@ -95,6 +166,6 @@ int main(){
     std::cout << ab; */
    // std::cout << cd;
 
-   std::cout << factorial(300);
+   std::cout << factorial(10);
     return 0;
 }
--- recursion/RecursionTest.cpp
+++ recursion/RecursionTest.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
+#include <vector>
 
-
+/**
+ * calculate fibunacci number of nth position recursively
+ */
 int fibunacci(int n){
     if(n <= 1)
         return n;
 
     return fibunacci(n - 1) + fibunacci(n - 2);
 }
+
+/**
+ * calculating the fibunacci number of nth position
+ * using the memoized solution
+ */
+int fibunacci(int n, std::vector<int> memo){
+    if(memo[n] != NULL)
+        return memo[n];
+
+    if(n <= 1)
+        return n;
+    
+    int result = fibunacci(n - 1) + fibunacci(n - 2);    
+    memo.push_back(result);
+    
+    return result;
+}
+
 
 double power(double base, int exponent){
     if(exponent == 0)
