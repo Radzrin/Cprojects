a8524e154b6dd73c23eb913bcb0242434a3ee388
Radzrin <60394053+Radzrin@users.noreply.github.com>
2026-05-20 22:57:37 -0400

    binary search

--- recursion/RecursionTest.cpp
+++ recursion/RecursionTest.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 /**
  * calculate fibunacci number of nth position recursively
@@ -16,8 +17,8 @@ int fibunacci(int n){
  * using the memoized solution
  */
 int fibunacci(int n, std::vector<int> memo){
-    if(memo[n] != NULL)
-        return memo[n];
+    if(memo.at(n) != NULL)
+        return memo.at(n);
 
     if(n <= 1)
         return n;
@@ -28,7 +29,10 @@ int fibunacci(int n, std::vector<int> memo){
     return result;
 }
 
-
+/**
+ * calculates the power of a number
+ * ex: 2^2 = 4
+ */
 double power(double base, int exponent){
     if(exponent == 0)
     return 1.0;
@@ -47,9 +51,40 @@ double power(double base, int exponent){
 }
 
 
+/**
+ * Write a recursive function that implements binary search on a sorted array. 
+ * The function should return the index of the target element if found, 
+ * or -1 if the element is not present in the array. 
+ * Your implementation should divide the search space in half with each recursive call.
+ * 2 [1, 2, 3, 4, 5, 6, 7, 8]
+ * [1, 2, 3, 4] [5, 6, 7, 8]
+ * [1, 2, 3, 4]
+ * [1, 2] [3, 4]
+ * [1, 2] 
+ * [1] [2]
+ */
+int binarySearch(int search, int nums[]){
+    int index = 0;
+    
+    if(search > nums[index]){
+    }else if(search < nums[index]){
+    }
+
+    if(search == nums[index])
+        return index;
+
+
+    return -1;
+}
+
+
 
 int main(){
    //std::cout << fibunacci(10) << "\n";
-    std::cout << power(2,10);
+    //std::cout << power(2,10);
+    int a[8] = {34, 2, 3, 4, 5, 6, 7, 8};
+
+    std::cout << binarySearch(2, a);
+
     return 0;
 }
